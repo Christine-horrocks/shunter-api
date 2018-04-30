728a0d82931fe43cfe20add2af7f34cf5eba232d
Christine-horrocks <splogy2001@hotmail.com>
2018-04-30 16:58:08 +0100

    Empty Home page serializer and home controller added

---
+++ app/serializers/page_serializer/home_page_serializer.rb
@@ -0,0 +1,26 @@
+module PageSerializer
+  class HomePageSerializer < PageSerializer::BasePageSerializer
+
+    def initialize()
+
+    end
+
+    def content
+      c = []
+      # c << "header with search bar"
+      # c << "welcome message and links to the old website"
+      # c << "object to a bill title, example, link to a blog"
+      # c << "search title, link and blog"
+      # c << "MPs and Lords information, links to MPs, Lords, Constituencies and Parties and groups"
+      # c << "Give feedback title and link"
+      # c << "Follow our progress title and link to blog"
+
+      c
+    end
+
+  def title
+    "beta.parliament.uk"
+  end
+
+  end
+end
