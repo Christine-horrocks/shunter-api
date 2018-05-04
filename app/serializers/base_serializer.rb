e60ecea7f9803ac964a8f47fa293da1e3e89df18
Christine-horrocks <splogy2001@hotmail.com>
2018-05-04 14:24:44 +0100

    home page title component serializer internationalized and tested

--- app/serializers/base_serializer.rb
+++ app/serializers/base_serializer.rb
@@ -4,6 +4,14 @@ class BaseSerializer
     content
   end
 
+  def t(dictionary_key)
+    I18n.t(dictionary_key)
+  end
+
+  def text_with_link(text_dictionary_key, link_text_dictionary_key, link )
+    I18n.t(text_dictionary_key, link: ActionController::Base.helpers.link_to(I18n.t(link_text_dictionary_key), link).html_safe).html_safe
+  end
+
   def content
     raise "You must call content through a specific class, you cannot cal lit through BaseSerializer."
   end
--- app/serializers/component_serializer/home_page_title_component_serializer.rb
+++ app/serializers/component_serializer/home_page_title_component_serializer.rb
@@ -7,16 +7,21 @@ module ComponentSerializer
 
     def data
       {
-        "title": I18n.t('.home.index.title'),
-        "prelink-text": "",
-        "link-text": I18n.t('.home.index.new_website'),
-        "link": "https://pds.blog.parliament.uk/2017/02/14/a-new-website-for-parliament-first-public-steps/",
-        "postlink-text": ""
-
+        "title": t('.home.index.title'),
+        "text-with-new-website-link": text_with_new_website_link,
+        "text-with-old-website-link": text_with_old_website_link
       }
-
     end
 
+    private
+
+    def text_with_new_website_link
+      text_with_link('.home.index.new_website', '.home.index.new_website_link', 'https://pds.blog.parliament.uk/2017/02/14/a-new-website-for-parliament-first-public-steps/')
+    end
+
+    def text_with_old_website_link
+      text_with_link('.home.index.old_website', '.home.index.old_website_link', 'https://www.parliament.uk')
+    end
 
   end
 end
