b696e938ae36c86c1ef07a124a612e1d6e2d4ff6
Christine-horrocks <splogy2001@hotmail.com>
2018-05-04 14:44:47 +0100

    home page subheading internationalized and tested

--- app/serializers/component_serializer/object_to_a_bill_component_serializer.rb
+++ app/serializers/component_serializer/object_to_a_bill_component_serializer.rb
@@ -10,9 +10,9 @@ module ComponentSerializer
         "subheading": "Object to a bill",
         "list-items": [
           {
-            "link-text": "Object to the Additional Provision to the High Speed Rail (West Midlands to Crew) Bill",
+            "link-text": t('.home.index.object_to_a_bill.object_to_link'),
             "link": "/petition-a-hybrid-bill/2",
-            "additional-text": "This deadline is 1pm on friday 27 April 2018"
+            "additional-text": t('.home.index.object_to_a_bill.object_to')
           }
         ],
         "blog-link": "https://pds.blog.parliament.uk/2018/02/09/building-a-new-online-service-petitioning-a-hybrid-bill/",
