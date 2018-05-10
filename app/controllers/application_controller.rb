3b195e8f650276460aca05856d46584851c79a12
WAZACZ <cazwazacz@gmail.com>
2018-05-10 11:44:23 +0100

    Create spec for render_json method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,13 +28,10 @@ class ApplicationController < ActionController::API
     raise ActionController::RoutingError, error.message
   end
 
-  def render_page(serializer)
-   response.headers['Content-Type'] = 'application/x-shunter+json'
-   response.headers['Access-Control-Allow-Origin'] = '*'
-   response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-   response.headers['Access-Control-Request-Method'] = '*'
-   response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-   render json: serializer.to_h
+  def render_page(serializer, response_parameter = response)
+    response_parameter.headers['Content-Type'] = 'application/x-shunter+json'
+
+    render json: serializer.to_h
   end
 
 end
