3b890409862e51c276444e973ad1dd9ecb2a4969
Christine-horrocks <splogy2001@hotmail.com>
2018-05-10 12:30:12 +0100

    base method specs in the right place

--- spec/serializers/component_serializer/base_component_serializer_spec.rb
+++ spec/serializers/component_serializer/base_component_serializer_spec.rb
@@ -9,20 +9,4 @@ describe ComponentSerializer::BaseComponentSerializer do
     end
   end
 
-  context '#t' do
-    it 'wraps the arguments for translation' do
-      args = 'text or link'
-      expect(subject.t(args)).to eq(I18n.t('text or link'))
-    end
-  end
-
-  context '#link_to' do
-    it 'wraps the arguments to create a text link' do
-      body = 'text or link'
-      url = '//link'
-      html_options = {}
-      expect(subject.link_to(body, url, html_options = {})).to eq("<a href=\"//link\">text or link</a>")
-    end
-  end
-
 end
