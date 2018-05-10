9a8f9888284be66337a113035f738ee265898a39
Christine-horrocks <splogy2001@hotmail.com>
2018-05-10 12:09:15 +0100

    base component serialiser with t() and link_to()

--- spec/serializers/component_serializer/base_component_serializer_spec.rb
+++ spec/serializers/component_serializer/base_component_serializer_spec.rb
@@ -1,10 +1,28 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::BaseComponentSerializer do
+
   context '#to_h' do
     it 'raises an error if called in BaseComponentSerializer' do
 
       expect { subject.to_h }.to raise_error("Name can only be called through a specific component serializer, not through the generic base component serializer")
     end
   end
+
+  context '#t' do
+    it 'wraps the arguments for translation' do
+      args = 'text or link'
+      expect(subject.t(args)).to eq(I18n.t('text or link'))
+    end
+  end
+
+  context '#link_to' do
+    it 'wraps the arguments to create a text link' do
+      body = 'text or link'
+      url = '//link'
+      html_options = {}
+      expect(subject.link_to(body, url, html_options = {})).to eq("<a href=\"//link\">text or link</a>")
+    end
+  end
+
 end
