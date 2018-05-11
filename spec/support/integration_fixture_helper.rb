af51ea7bc4c0831380bbc45758e226f6b0707af1
WAZACZ <cazwazacz@gmail.com>
2018-05-11 16:15:12 +0100

    Change to use single line File.open

--- spec/support/integration_fixture_helper.rb
+++ spec/support/integration_fixture_helper.rb
@@ -2,7 +2,6 @@ module IntegrationFixtureHelper
   def get_fixture(path)
     file_path = File.join(File.dirname(__FILE__), '../', 'fixtures', 'controllers', path)
 
-    file = File.open(file_path, 'r')
-    file.read
+    File.open(file_path, 'r') { |f| f.read }
   end
 end
