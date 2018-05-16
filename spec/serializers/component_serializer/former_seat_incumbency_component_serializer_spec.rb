b7d65f7c78626b3cec02a56051e0ddc51941a131
WAZACZ <cazwazacz@gmail.com>
2018-05-16 15:31:36 +0100

    Update FormerSeatIncumbenciesComponentSerializer spec

--- spec/serializers/component_serializer/former_seat_incumbency_component_serializer_spec.rb
+++ spec/serializers/component_serializer/former_seat_incumbency_component_serializer_spec.rb
@@ -2,16 +2,18 @@ require_relative '../../rails_helper'
 
 
 describe ComponentSerializer::FormerSeatIncumbenciesComponentSerializer do
-  let (:member) { double('member', display_name: 'Former MP', graph_id: "121212")}
-  let (:seat_incumbency2) {double('seat_incumbency2', current?: false, member: member, date_range: "121212" )}
-  let (:seat_incumbency1) {double('seat_incumbency1', current?: true )}
-  let (:seat_incumbencies) { [seat_incumbency1, seat_incumbency2] }
-  let (:formerseatincumbenciescomponentserializer) { described_class.new(seat_incumbencies) }
+  let(:member) { double('member', display_name: 'Former MP', graph_id: "121212") }
+  let(:seat_incumbency2) { double('seat_incumbency2', current?: false, member: member, date_range: "121212" ) }
+  let(:seat_incumbency1) { double('seat_incumbency1', current?: true ) }
+  let(:seat_incumbencies) { [seat_incumbency1, seat_incumbency2] }
+  let(:former_seat_incumbencies_component_serializer) { described_class.new(seat_incumbencies) }
 
   context '#to_h' do
 
     it 'returns a hash containing the name and data for the former seat incumbencies' do
-      expect(formerseatincumbenciescomponentserializer.to_h).to eq({:name=>"former-seat-incumbencies", :data=>{:title=>"Former MPs", :"former-seat-incumbency-list"=>[{:display_name=>"Former MP", :graph_id=>"121212", :date_range=>"121212"}]}})
+      expected = get_fixture('component_serializer/former_seat_incumbencies_component_serializer/fixture.yml')
+
+      expect(former_seat_incumbencies_component_serializer.to_yaml).to eq expected
   end
  end
 end
