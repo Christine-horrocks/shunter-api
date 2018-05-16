f1d8b092c9e3d15e5ffd64eaacae3898d4350398
WAZACZ <cazwazacz@gmail.com>
2018-05-16 16:16:30 +0100

    Update RolesComponent spec

--- spec/serializers/component_serializer/roles_component_serializer_spec.rb
+++ spec/serializers/component_serializer/roles_component_serializer_spec.rb
@@ -1,23 +1,33 @@
 require_relative '../../rails_helper'
 
-describe ComponentSerializer::RolesComponentSerializer do
-
-  let (:history) { {current: true} }
-  let (:role_helper) {double('role_helper', create_role_history: history, organise_roles: "This is your history")}
-  let (:rolescomponentserializer) { described_class.new(1, 2, 3, 4, role_helper) }
-
-  context '#to_h' do
-    it 'returns a hash containing the name and data for MPs roles' do
-      expect(rolescomponentserializer.to_h).to eq({ name: "roles", data: { template: "roles", "role-list": "This is your history"}})
-    end
-  end
+RSpec.describe ComponentSerializer::RolesComponentSerializer do
+  let(:history) { { current: true } }
+  let(:role_helper_double) { double('role_helper_double', create_role_history: history, organise_roles: 'hello') }
+  let(:serializer) { described_class.new(1, 2, 3, 4, role_helper_double) }
 
   context '#current_roles' do
-    it 'is called with create_role_history and organise_roles' do
-      rolescomponentserializer.to_h
-      expect(role_helper).to have_received(:create_role_history)
-      expect(role_helper).to have_received(:organise_roles)
+    context 'history[:current] is true' do
+      it 'role_helper receives create_role_history and organise_roles' do
+        serializer.to_h
+        expect(role_helper_double).to have_received(:create_role_history)
+        expect(role_helper_double).to have_received(:organise_roles)
+      end
+
+      it 'returns the roles data' do
+        expected = get_fixture('component_serializer/roles_component_serializer/fixture.yml')
+
+        expect(serializer.to_yaml).to eq expected
+      end
+    end
+
+    context 'history[:current] is false' do
+      let(:history) { { current: false } }
+
+      it 'role_helper does not receive organise_roles if history[:current] is false' do
+        serializer.to_h
+        expect(role_helper_double).to have_received(:create_role_history)
+        expect(role_helper_double).to_not have_received(:organise_roles)
+      end
     end
   end
-
 end
