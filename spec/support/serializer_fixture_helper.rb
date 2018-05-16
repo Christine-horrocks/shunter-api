196645463f2c601b4b84e67bfb59ab07860cdd4b
WAZACZ <cazwazacz@gmail.com>
2018-05-16 17:18:25 +0100

    Methods chomp '_spec.rb' from file path

--- spec/support/serializer_fixture_helper.rb
+++ spec/support/serializer_fixture_helper.rb
@@ -2,14 +2,12 @@ require 'fileutils'
 
 module SerializerFixtureHelper
   def get_fixture(filename, path)
-    directory_name = path.chomp('.rb').split('/').insert(6, 'fixtures').join('/')
-    # file_path = File.join(File.dirname(__FILE__), '../', 'fixtures', 'serializers', path)
-
+    directory_name = path.chomp('_spec.rb').split('/').insert(6, 'fixtures').join('/')
     File.open("#{directory_name}/#{filename}.yml", 'r') { |f| f.read }
   end
 
   def create_fixture(serializer, filename, path)
-    directory_name = path.chomp('.rb').split('/').insert(6, 'fixtures').join('/')
+    directory_name = path.chomp('_spec.rb').split('/').insert(6, 'fixtures').join('/')
 
     Dir.mkdir(directory_name) unless File.exists?(directory_name)
 
