fae4d0d4dca220a5acd077290b89206837938f8b
WAZACZ <cazwazacz@gmail.com>
2018-05-16 15:37:51 +0100

    Update HeroComponentSerializer spec

--- spec/serializers/component_serializer/hero_component_serializer_spec.rb
+++ spec/serializers/component_serializer/hero_component_serializer_spec.rb
@@ -1,13 +1,16 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::HeroComponentSerializer do
-  let ( :title ) { 'Welcome to Parliament' }
-  let ( :additional_text ) { 'Even more interesting information' }
+  let(:title) { 'Welcome to Parliament' }
+  let(:additional_text) { 'Even more interesting information' }
 
-  let ( :herocomponentserializer ) { described_class.new(title, additional_text) }
+  let(:hero_component_serializer) { described_class.new(title, additional_text) }
+
   context '#to_h' do
     it 'returns a hash containing the name and data' do
-      expect(herocomponentserializer.to_h).to eq({:name=>"hero", :data=>{:title=>"Welcome to Parliament", :"additional-text"=>["Even more interesting information"]}})
+      expected = get_fixture('component_serializer/hero_component_serializer/fixture.yml')
+
+      expect(hero_component_serializer.to_yaml).to eq expected
     end
   end
 end
