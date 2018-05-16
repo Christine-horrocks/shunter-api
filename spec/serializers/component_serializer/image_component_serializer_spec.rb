16feb79417282d402e1d18e0b741ea203a92c871
WAZACZ <cazwazacz@gmail.com>
2018-05-16 15:45:06 +0100

    Update ImageComponentSerializer spec

--- spec/serializers/component_serializer/image_component_serializer_spec.rb
+++ spec/serializers/component_serializer/image_component_serializer_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::ImageComponentSerializer do
-  let (:object) { double('object')}
-  let (:imagecomponentserializer) { described_class.new(object) }
+  let(:object) { double('object', graph_id: '43RHonMf', image_id: '43RHonMf', display_name: 'Dianne Abbott')}
+  let(:image_component_serializer) { described_class.new(object) }
+
   context '#to_h' do
     it 'returns a hash containing the name and data for the image' do
-      allow(object).to receive(:graph_id) {"43RHonMf"}
-      allow(object).to receive(:image_id) {"43RHonMf"}
-      allow(object).to receive(:display_name) {"Dianne Abbott"}
-      expect(imagecomponentserializer.to_h).to eq({:name=>"image", :data=>{:template=>"person-image", :"figure-url"=>"/media/43RHonMf", :"image-srcset1"=>"https://api.parliament.uk/Staging/photo/43RHonMf.jpeg?crop=CU_5:2&width=732&quality=80, https://api.parliament.uk/Staging/photo/43RHonMf.jpeg?crop=CU_5:2&width=1464&quality=80 2x", :"image-srcset2"=>"https://api.parliament.uk/Staging/photo/43RHonMf.jpeg?crop=MCU_3:2&width=444&quality=80, https://api.parliament.uk/Staging/photo/43RHonMf.jpeg?crop=MCU_3:2&width=888&quality=80 2x", :"image-src"=>"https://api.parliament.uk/Staging/photo/43RHonMf.jpeg?crop=CU_1:1&width=186&quality=80", :"image-alt"=>"Dianne Abbott"}})
+      expected = get_fixture('component_serializer/image_component_serializer/fixture.yml')
+
+      expect(image_component_serializer.to_yaml).to eq expected
     end
   end
 end
