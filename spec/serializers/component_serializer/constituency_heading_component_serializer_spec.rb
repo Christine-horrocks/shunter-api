c13d513ef16c731222da9af69adfdfcdbae02b01
WAZACZ <cazwazacz@gmail.com>
2018-05-16 12:47:42 +0100

    Update ConstituencySubheadingComponentSerializer spec

--- spec/serializers/component_serializer/constituency_heading_component_serializer_spec.rb
+++ spec/serializers/component_serializer/constituency_heading_component_serializer_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::ConstituencyHeadingComponentSerializer do
-  let ( :constituency ) { double('constituency', name: 'Blyth Vally') }
-  let ( :constituency_heading_component_serializer ) { described_class.new(constituency) }
+  let (:constituency) { double('constituency', name: 'Blyth Vally') }
+  let (:constituency_heading_component_serializer) { described_class.new(constituency) }
 
   context '#to_h' do
     it 'returns a hash containing the name and data' do
