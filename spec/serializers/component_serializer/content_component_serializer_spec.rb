b9d1657fe28ec31ff54ce24d4e9d241b5acbae2c
WAZACZ <cazwazacz@gmail.com>
2018-05-16 14:49:48 +0100

    Update ContentComponentSerializer spec

--- spec/serializers/component_serializer/content_component_serializer_spec.rb
+++ spec/serializers/component_serializer/content_component_serializer_spec.rb
@@ -1,14 +1,16 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::ContentComponentSerializer do
-  let ( :heading ) { 'Important things' }
-  let ( :additional_text ) { 'Even more interesting information' }
+  let(:heading) { 'Important things' }
+  let(:additional_text) { 'Even more interesting information' }
 
-  let ( :contentcomponentserializer ) { described_class.new(heading, additional_text) }
+  let (:content_component_serializer) { described_class.new(heading, additional_text) }
+
   context '#to_h' do
     it 'returns a hash containing the name and data' do
-      expect(contentcomponentserializer.to_h).to eq({:name=>"content", :data=>{:heading=>"Important things", :"additional-text"=>["Even more interesting information"]}})
+      expected = get_fixture('component_serializer/content_component_serializer/fixture.yml')
+
+      expect(content_component_serializer.to_h).to be_the_same_in_yaml_as expected
     end
   end
-
 end
