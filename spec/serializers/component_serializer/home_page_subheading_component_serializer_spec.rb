569d318cab4b8f1970f68510848564d842f75b5e
WAZACZ <cazwazacz@gmail.com>
2018-05-16 15:41:22 +0100

    Update HomePageSubheadingComponent spec

--- spec/serializers/component_serializer/home_page_subheading_component_serializer_spec.rb
+++ spec/serializers/component_serializer/home_page_subheading_component_serializer_spec.rb
@@ -1,11 +1,12 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::HomePageSubheadingComponentSerializer do
-
   context '#to_h' do
     it 'returns a hash containing the name and data' do
-      expect(subject.to_h).to eq({:name=>"home-page-subheading", :data=>{:subheading=>"What's on beta.parliament.uk"}})
+      create_fixture(subject.to_h, 'spec/fixtures/serializers/component_serializer/home_page_subheading_component_serializer/fixture.yml')
+      expected = get_fixture('component_serializer/home_page_subheading_component_serializer/fixture.yml')
+
+      expect(subject.to_yaml).to eq expected
     end
   end
-
 end
