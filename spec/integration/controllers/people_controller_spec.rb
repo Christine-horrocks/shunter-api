302a15cb9688b779d631bb97b30692a58e56e148
Allan Wazacz <cazwazacz@gmail.com>
2018-05-18 11:59:12 +0100

    Remove unneeded before hooks

--- spec/integration/controllers/people_controller_spec.rb
+++ spec/integration/controllers/people_controller_spec.rb
@@ -3,58 +3,42 @@ require_relative '../../rails_helper'
 RSpec.describe PeopleController, vcr: true do
   describe 'GET show' do
     context 'with current MP' do
-      before(:each) do
+      it 'renders expected JSON output' do
         get '/people/43RHonMf'
-      end
 
-      context 'with all the information present' do
-        it 'renders expected JSON output' do
-          expected_json = get_fixture('show', 'with_all_data')
+        expected_json = get_fixture('show', 'with_all_data')
 
-          expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
-        end
+        expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
       end
     end
 
-    context 'with a former MP' do
-      before(:each) do
+    context 'with former MP' do
+      it 'renders expected JSON output' do
         get '/people/qkVSY7jb'
-      end
 
-      context 'with all the information present' do
-        it 'renders expected JSON output' do
-          expected_json = get_fixture('show', 'former_mp')
+        expected_json = get_fixture('show', 'former_mp')
 
-          expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
-        end
+        expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
       end
     end
 
-    context 'with a current Lord' do
-      before(:each) do
+    context 'with current Lord' do
+      it 'renders expected JSON output' do
         get '/people/0G6VnY9o'
-      end
 
-      context 'with all the information present' do
-        it 'renders expected JSON output' do
-          expected_json = get_fixture('show', 'current_lord')
+        expected_json = get_fixture('show', 'current_lord')
 
-          expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
-        end
+        expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
       end
     end
 
-    context 'with a former Lord' do
-      before(:each) do
+    context 'with former Lord' do
+      it 'renders expected JSON output' do
         get '/people/c2Q5Jyop'
-      end
 
-      context 'with all the information present' do
-        it 'renders expected JSON output' do
-          expected_json = get_fixture('show', 'former_lord')
+        expected_json = get_fixture('show', 'former_lord')
 
-          expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
-        end
+        expect(JSON.parse(response.body).to_yaml).to eq(expected_json)
       end
     end
   end
