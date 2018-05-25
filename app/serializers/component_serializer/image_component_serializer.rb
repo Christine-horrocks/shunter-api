04fa7285a02b3fdcc726c248914d779fcfe7efd2
WAZACZ <cazwazacz@gmail.com>
2018-05-25 17:18:33 +0100

    Update ImageComponentSerializer to have a default css class

--- app/serializers/component_serializer/image_component_serializer.rb
+++ app/serializers/component_serializer/image_component_serializer.rb
@@ -1,11 +1,12 @@
 module ComponentSerializer
   # Creates a hash containing all elements needed to produce a responsive image.
   class ImageComponentSerializer < BaseComponentSerializer
+    AVATAR_ROUND = 'avatar--round'.freeze
     # Initialise an image component with a hash of image data.
     #
     # @param [Hash] image_data a hash of image data.
     # @example { figure_url: 'data', image_srcset1: 'data', image_srcset2: 'data', image_src: 'data', image_alt: 'data'}
-    def initialize(image_data)
+    def initialize(image_data, css_class = ComponentSerializer::ImageComponentSerializer::AVATAR_ROUND)
       @figure_url = image_data[:figure_url]
 
       @image_srcset1 = image_data[:image_srcset1]
@@ -13,12 +14,14 @@ module ComponentSerializer
 
       @image_src = image_data[:image_src]
       @image_alt = image_data[:image_alt]
+
+      @css_class = css_class
     end
 
     private
 
     def name
-      "image"
+      'image'
     end
 
     def data
@@ -29,7 +32,9 @@ module ComponentSerializer
           "image-srcset2": @image_srcset2,
 
           "image-src": @image_src,
-          "image-alt": @image_alt
+          "image-alt": @image_alt,
+
+          "css-class": @css_class
       }
     end
 
