09866e15ca4ddc8e24da2b76bf8181f84f3528e9
WAZACZ <cazwazacz@gmail.com>
2018-05-25 16:17:08 +0100

    Update ImageComponentSerializer

--- spec/serializers/component_serializer/image_component_serializer_spec.rb
+++ spec/serializers/component_serializer/image_component_serializer_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::ImageComponentSerializer do
-  let(:object) { double('object', graph_id: '43RHonMf', image_id: '43RHonMf', display_name: 'Dianne Abbott')}
-  let(:image_component_serializer) { described_class.new(object) }
+  let(:image_data) {
+    { 'figure_url': 'figure_url',
+      'image_srcset1': 'image_srcset1',
+      'image_srcset2': 'image_srcset2',
+      'image_src': 'image_src',
+      'image_alt': 'image_alt'
+    }
+  }
+
+  let(:image_component_serializer) { described_class.new(image_data) }
 
   context '#to_h' do
     it 'returns a hash containing the name and data for the image' do
