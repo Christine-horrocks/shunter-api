699ce0373b3b2a63881246b4d5db1c3782253f5b
WAZACZ <cazwazacz@gmail.com>
2018-05-25 14:42:57 +0100

    Update ListComponentSerializer

--- app/serializers/component_serializer/list_component_serializer.rb
+++ app/serializers/component_serializer/list_component_serializer.rb
@@ -1,19 +1,33 @@
 module ComponentSerializer
-  class ListComponentSerializer < BaseSerializer
-    def initialize(heading, list_items)
-      @heading = heading
-      @list_items = list_items
+  # Creates a list using the array of objects passed to it. Can be used with atoms and components, inculding cards.
+  class ListComponentSerializer < BaseComponentSerializer
+    UL = 'ul'.freeze
+    OL = 'ol'.freeze
+    # Initialise a list component with an array of objects.
+    #
+    # @param [Array<Hash>] components an array of objects, each object is a component or atom.
+    # @example [{"heading": 'This is a heading'}, {"paragraph": 'This is a paragraph'}]
+    # @param [String] css_class containing -- and the type of css class for the list.
+    # @param [String] list_type indicating if the list is a ordered or unordered list, with a defaultof unordered.
+    def initialize(components, css_class = '--pipe', list_type=ComponentSerializer::ListComponentSerializer::UL)
+      @components = components
+      @css_class = css_class
+      @list_type = list_type
     end
 
-    def to_h
+    private
+
+    def name
+      'list'
+    end
+
+    def data
       {
-        "name": 'list-component',
-        "data":
-        {
-          "heading": @heading,
-          "list-items": @list_items
-        }
+          'css-class': @css_class,
+          'list-type': @list_type,
+          'components': @list_items
       }
     end
+
   end
 end
--- spec/serializers/component_serializer/list_component_serializer_spec.rb
+++ spec/serializers/component_serializer/list_component_serializer_spec.rb
@@ -1,10 +1,9 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::ListComponentSerializer do
-  let(:heading) { '/mps' }
   let(:list_items) { [1, 2, 3] }
 
-  let(:list_component_serializer) { described_class.new(heading, list_items) }
+  let(:list_component_serializer) { described_class.new(list_items) }
 
   context '#to_h' do
     it 'returns a hash containing the name and data' do
