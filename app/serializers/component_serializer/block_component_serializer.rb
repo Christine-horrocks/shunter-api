00a6a0ee3c9575db02eacb45a5b56e10c36aebab
WAZACZ <cazwazacz@gmail.com>
2018-05-25 14:18:48 +0100

    Add BlockComponentSerializer

---
+++ app/serializers/component_serializer/block_component_serializer.rb
@@ -0,0 +1,29 @@
+module ComponentSerializer
+  # Creates a hash with the name block and data which comprises of another hash containing a css class, which can be used to chnage the type of block, and an array of hashes.
+  class BlockComponentSerializer <  BaseComponentSerializer
+    BLOCK = 'block'.freeze
+    # Initialise a block component with an array of objects.
+    #
+    # @param [Array<Hash>] components an array of objects, each object is a component or atom.
+    # @example [{"title": 'Here is my title', "text": 'Here is my text'}, {"title": 'Here is my title', "text": 'Here is my text'}].
+    # @param [String] css_class contains the class type of the block, with a default of block.
+    def initialize(components, css_class = ComponentSerializer::BlockComponentSerializer::BLOCK)
+      @components = components
+      @css_class = css_class
+    end
+
+    private
+
+    def name
+      'block'
+    end
+
+    def data
+      {
+          "css-class": @css_class,
+          "components": @components
+      }
+    end
+
+  end
+end
