aa883f9bb553a51b5df70c0aba1040d6999b2fed
Allan Wazacz <cazwazacz@gmail.com>
2018-05-29 12:59:53 +0100

    Update PostcodesShowPageSerializer spec

--- spec/serializers/page_serializer/postcodes_show_page_serializer_spec.rb
+++ spec/serializers/page_serializer/postcodes_show_page_serializer_spec.rb
@@ -35,5 +35,66 @@ describe PageSerializer::PostcodesShowPageSerializer, vcr: false do
         expect(serializer.to_yaml).to eq expected
       end
     end
+
+    context 'calling the correct serializers' do
+      it '#sub_components' do
+        allow(serializer).to receive(:card_components) { 'card_components' }
+
+        allow(ComponentSerializer::HeadingComponentSerializer).to receive(:new)
+        allow(ComponentSerializer::BlockComponentSerializer).to receive(:new)
+        allow(ComponentSerializer::ListComponentSerializer).to receive(:new)
+        allow(ComponentSerializer::ParagraphComponentSerializer).to receive(:new)
+
+        serializer.to_h
+
+        expect(ComponentSerializer::HeadingComponentSerializer).to have_received(:new).with(
+          '<span>Results for EH6 5EZ</span>', 1
+        )
+
+        expect(ComponentSerializer::BlockComponentSerializer).to have_received(:new).with(
+          [ComponentSerializer::HeadingComponentSerializer.new("<a href='/constituencies/123'>Constituency</a>", 2).to_h]
+        )
+
+        expect(ComponentSerializer::ListComponentSerializer).to have_received(:new).with(
+          [ComponentSerializer::CardComponentSerializer.new('card_components').to_h],
+          '--block'
+        )
+
+        expect(ComponentSerializer::ParagraphComponentSerializer).to have_received(:new).with(
+          ["<a href='/postcodes'>Check for a different postcode</a>"]
+        )
+      end
+
+      it '#card_components' do
+        allow(serializer).to receive(:image_data) { 'image_data' }
+        allow(serializer).to receive(:card_details_components) { 'card_details_components' }
+
+        allow(ComponentSerializer::ImageComponentSerializer).to receive(:new)
+        allow(ComponentSerializer::CardDetailsComponentSerializer).to receive(:new)
+
+        serializer.to_h
+
+        expect(ComponentSerializer::ImageComponentSerializer).to have_received(:new).with('image_data')
+        expect(ComponentSerializer::CardDetailsComponentSerializer).to have_received(:new).with('card_details_components')
+      end
+
+      it '#card_details_components' do
+        allow(ComponentSerializer::HeadingComponentSerializer).to receive(:new)
+        allow(ComponentSerializer::ParagraphComponentSerializer).to receive(:new)
+
+        serializer.to_h
+
+        expect(ComponentSerializer::HeadingComponentSerializer).to have_received(:new).with(
+            "<a href='/people/321'>Display Name</a>", 2
+        )
+
+        expect(ComponentSerializer::ParagraphComponentSerializer).to have_received(:new).with(
+            [
+                'MP for Constituency',
+                'Labour'
+            ]
+        )
+      end
+    end
   end
 end
